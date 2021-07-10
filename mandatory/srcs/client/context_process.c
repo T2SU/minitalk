931ec5aea5496143225dd79d9946e198740560bb
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-07-11 01:27:31 +0900

    mandatory

--- mandatory/srcs/client/context_process.c
+++ mandatory/srcs/client/context_process.c
@@ -6,7 +6,7 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 21:56:39 by smun              #+#    #+#             */
-/*   Updated: 2021/07/11 01:19:35 by smun             ###   ########.fr       */
+/*   Updated: 2021/07/11 01:26:17 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,11 @@
 
 void	context_process(t_context *ctx)
 {
-	const pid_t	pid = ctx->opponent;
-
 	if (ctx->mode == kClient)
 	{
 		ft_putstr("Sent successfully ");
 		ft_putnbr(ctx->data_len);
 		ft_putstr(" bytes!\n");
-		ctx->data = NULL;
-		context_register(kServer);
-		ctx->opponent = pid;
-		ctx->data_expected_len = sizeof(unsigned int);
-		kill(pid, SIGUSR1);
-	}
-	else
-	{
-		ft_putstr("Received crc32: ");
-		ft_putnbr(*(int *)(&ctx->data[0]));
-		ft_putstr("\n");
-		//kill(pid, SIGUSR1);
 		ft_exit(EXIT_SUCCESS, NULL);
 	}
 }
--- mandatory/srcs/server/context_process.c
+++ mandatory/srcs/server/context_process.c
@@ -6,7 +6,7 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 21:11:26 by smun              #+#    #+#             */
-/*   Updated: 2021/07/11 01:22:10 by smun             ###   ########.fr       */
+/*   Updated: 2021/07/11 01:25:18 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,10 @@
 
 void	context_process(t_context *ctx)
 {
-	const pid_t		pid = ctx->opponent;
-	unsigned int	crc;
-
 	if (ctx->mode == kServer)
 	{
 		write(STDOUT_FILENO, ctx->data, (ctx->data_idx / 8) - 1);
-		crc = ft_crc32(ctx->data, ctx->data_idx / 8, 0);
-		context_register(kClient);
-		if (!buffer_expand(&ctx->data, 0, 4))
-			ft_exit(EXIT_FAILURE, "Out of memory");
-		*(unsigned int *)(&ctx->data[0]) = crc;
-		ctx->data_len = sizeof(unsigned int);
-		ctx->data_idx = 0;
-		ctx->opponent = pid;
+		context_reset(ctx, 0);
 		ft_putstr("\n");
-	}
-	else
-	{
-		ft_putstr("Sent crc32 ack: ");
-		ft_putnbr(*(int *)(&ctx->data[0]));
-		ft_putstr("\n");
-		kill(pid, SIGUSR1);
-		context_register(kServer);
 	}
 }
