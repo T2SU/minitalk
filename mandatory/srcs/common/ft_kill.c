42a503961ff1512087538c70403f94747ff201dc
Tensiya <40755203+T2SU@users.noreply.github.com>
2021-07-13 18:17:05 +0900

    give up (heart)

--- mandatory/srcs/common/ft_kill.c
+++ mandatory/srcs/common/ft_kill.c
@@ -6,7 +6,7 @@
 /*   By: smun <smun@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/13 14:49:34 by smun              #+#    #+#             */
-/*   Updated: 2021/07/13 14:51:36 by smun             ###   ########.fr       */
+/*   Updated: 2021/07/13 18:13:18 by smun             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,6 @@ void	ft_kill(const pid_t pid, int signal)
 {
 	const int	ret_value = kill(pid, signal);
 
-	if (ret_value == -1)
+	if (ret_value != 0)
 		ft_exit(EXIT_FAILURE, "Failed to send signal");
 }
